e815708469448f43f16e47974d8dcfe91cc07b65
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2015-02-03 16:45:37 +0100

    Add directory handling

--- spacebars-compiler.js
+++ spacebars-compiler.js
@@ -1,7 +1,7 @@
-var input,output,i;
+var input,output,dir,i;
 
 function showHelp() {
-	console.log("Usage: spacebars-compiler --input template.hbs --output template.js");
+	console.log("Usage: spacebars-compiler [--input template.hbs] [--dir directory]  --output template.js");
 };
 
 for(i=0;i<process.argv.length-1;++i) { 
@@ -10,6 +10,10 @@ for(i=0;i<process.argv.length-1;++i) {
 		case "-i":
 			input = process.argv[++i];
 			break;
+		case "--dir":
+		case "-d":
+			dir = process.argv[++i];
+			break;
 		case "--output":
 		case "-o":
 			output = process.argv[++i];
@@ -17,10 +21,11 @@ for(i=0;i<process.argv.length-1;++i) {
 	}
 }
 
-if (!input || !output)
+if ((!input && !dir) || !output)
 	return showHelp();
 
 var fs       = require("fs");
+var path     = require("path");
 var lodash   = require("lodash");
 var Meteor   = require("meteor-core")(lodash);
 require("meteor-htmljs")(Meteor);
@@ -28,19 +33,54 @@ require("meteor-html-tools")(Meteor);
 require("meteor-blaze-tools")(Meteor);
 require("meteor-spacebars-compiler")(Meteor);
 
-fs.readFile(input, 'utf8', function (err,data) {
-	if (err) {
-		return console.log("Error opening input template file " +err);
-	}
-	var js = "module.exports = function(Meteor) {\r\n";
-        js += "	var HTML = Meteor.HTML;\r\n";
-        js += "	var Blaze = Meteor.Blaze;\r\n";
-        js += "	var Spacebars = Meteor.Spacebars;\r\n";
-	js += "	return " + Meteor.SpacebarsCompiler.compile(data, {isTemplate: true}) + ";\r\n";
-	js += "};\r\n";	
-	fs.writeFile(output,js,{flags:'w'}, function(err) {
-		if(err) 
-			return console.log("Error writing output js file "+ err);
-		console.log("done!");
-	}); 
-});
+if (input)
+	fs.readFile(input, 'utf8', function (err,data) {
+		if (err) 
+			return console.log("Error opening input template file " +err);
+		var js = "module.exports = function(Meteor) {\r\n";
+		js += "	var HTML = Meteor.HTML;\r\n";
+		js += "	var Blaze = Meteor.Blaze;\r\n";
+		js += "	var Spacebars = Meteor.Spacebars;\r\n";
+		js += "	return " + Meteor.SpacebarsCompiler.compile(data, {isTemplate: true}) + ";\r\n";
+		js += "};\r\n";	
+		//Write JS to file
+		fs.writeFile(output,js,{flags:'w'}, function(err) {
+			if(err) 
+				return console.log("Error writing output js file "+ err);
+			console.log("done!");
+		}); 
+	});
+else if (dir)
+	fs.readdir(dir, function(err, files) {
+		if (err) 
+			return console.log("Error accessing directory with input template files " +err);
+		var js = "module.exports = function(Meteor) {\r\n";
+		js += "	var HTML = Meteor.HTML;\r\n";
+		js += "	var Blaze = Meteor.Blaze;\r\n";
+		js += "	var Spacebars = Meteor.Spacebars;\r\n";
+		js += "	var templates = {};\r\n";
+		//For each file in dir
+		for(i=0;i<files.length;++i) {
+			//Get file name
+			var file = files[i];
+			//if it is a template
+			if (path.extname(file)===".hbs") {
+				//Get template name
+				var template = path.basename(file,".hbs");
+				//Log
+				console.log("appending template '" + template +"'");
+				//Read data
+				var data = fs.readFileSync(dir+"/"+file, 'utf8');
+				//Compile and generate entry in template map
+				js += "	templates['" + template + "'] = " + Meteor.SpacebarsCompiler.compile(data, {isTemplate: true}) + ";\r\n";
+			}
+		}
+		js += "	return templates;\r\n";
+		js += "};\r\n";	
+		//Write JS to file
+		fs.writeFile(output,js,{flags:'w'}, function(err) {
+			if(err) 
+				return console.log("Error writing output js file "+ err);
+			console.log("done!");
+		}); 
+	});
